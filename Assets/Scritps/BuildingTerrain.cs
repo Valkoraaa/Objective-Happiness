fe2b495bafae3e4a0313cdc03d9824d25d4927f8
Valkoraaa <bb4947659@gmail.com>
2025-12-03 13:54:34 +0100

    feat : construction

--- Assets/Scritps/BuildingTerrain.cs
+++ Assets/Scritps/BuildingTerrain.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BuildingTerrain : MonoBehaviour
 {
+    public GameObject school;
+    public GameObject farm;
+    public GameObject museum;
+    public GameObject library;
+    public GameObject house;
+    private GameObject choosenBuilding;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,20 @@ public class BuildingTerrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Vérifie le tag de l’objet touché
+                if (hit.collider.CompareTag("constructible"))
+                {
+                    Debug.Log("Objet constructible cliqué : " + hit.collider.name);
+                    Instantiate(school, hit.collider.transform.position, Quaternion.identity);
+                }
+            }
+        }
     }
 }
